a3d9ac07ef1aaf231f10480a97ff4255ba7ef316
Bareera Bilal <bareerabilal03@gmail.com>
2025-09-21 19:57:27 +0530

    Add files via upload

---
+++ Mirarqah-Architectural-Studio/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mirarqah_Architectural_Studio.Models;
+
+namespace Mirarqah_Architectural_Studio.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Mirarqah-Architectural-Studio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mirarqah_Architectural_Studio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
