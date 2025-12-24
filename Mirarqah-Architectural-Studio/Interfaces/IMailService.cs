cb0fbbbed7958e3b3e02b8a747f64fce0385c3e7
Bareera Bilal <bareerabilal03@gmail.com>
2025-12-24 18:54:12 +0530

    Delete Mirarqah-Architectural-Studio directory

--- Mirarqah-Architectural-Studio/Interfaces/IMailService.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace P1WEBMVC.Interfaces;
-
-public interface IMailService
-{
-public Task SendEmailAsync(string emailAddress , string subject ,  string body, bool isHtml = true );
-}
--- Mirarqah-Architectural-Studio/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mirarqah_Architectural_Studio.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
